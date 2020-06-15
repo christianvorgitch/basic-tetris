50ac101da9b18789731d484b765144cfb444be8d
christian vorgitch <vorgitchchristian@gmail.com>
2020-06-15 00:55:18 -0500

    move left
    added some of the move left logic

--- app.js
+++ app.js
@@ -90,6 +90,19 @@ function freeze() {
 	}
 }
 
+// move the tetromino left, unless at the edge or it is blocked
+function moveLeft () {
+	undraw()
+	const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
+
+	if(!isAtLeftEdge) currentPosition -=1
+
+	if(current.some (index => squares[currentPosition + index].classList.contains('taken'))) {
+		currentPosition +=1
+	}
+
+	draw()
+}
 
 
 
